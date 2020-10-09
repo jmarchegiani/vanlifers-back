e5c13df75ba49b7d20a6fd8511d0a7b10420839d
Johny ! <juanpa.mgi@gmail.com>
2020-10-09 13:57:15 -0300

    Creds fix

--- app.js
+++ app.js
@@ -9,6 +9,23 @@ var app = express();
 
 // Middleware
 app.use(express.json());
+app.use((req, res, next) => {
+    if (req.method === 'POST' || req.method === 'PUT' || req.method === 'DELETE') {
+        const auth = {login : 'vanlifers', password : 'eravo384agb74a3b7aTH'}
+
+        const b64auth = (req.headers.authorization || '').split(' ')[1] || '';
+        const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':');
+
+        if (login && password && login === auth.login && password === auth.password) {
+            // Access granted...
+            return next()
+        }
+        res.set('WWW-Authenticate', 'Basic realm="401"');
+        res.status(401).send('Authentication required.');
+    }
+    return next();
+})
+
 //app.use(bodyParser.urlencoded({extended:false}));
 
 
